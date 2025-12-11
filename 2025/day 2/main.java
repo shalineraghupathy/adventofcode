969d6b2c44e4f8705eef776b8485425b2bddf56e
shalineraghupathy <shalineraghupathy02@gmail.com>
2025-12-11 11:47:33 +0000

    day 2 part 2

--- 2025/day 2/main.java
+++ 2025/day 2/main.java
@@ -1,14 +1,34 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class main {
 
-    static boolean isDouble(String s){
-        int l = s.length();
-        if(l%2 != 0) return false;
-        String right = s.substring(0,l/2);
-        String left = s.substring(l/2);
-        return right.equals(left);
+    // static boolean isDouble(String s){
+    //     int l = s.length();
+    //     if(l%2 != 0) return false;
+    //     String right = s.substring(0,l/2);
+    //     String left = s.substring(l/2);
+    //     return right.equals(left);
+    // }
+
+    static boolean isInvalid(String s) {
+    int L = s.length();
+    for (int k = 1; k <= L/2; k++) { 
+        if (L % k != 0) continue;
+        String segment = s.substring(0, k);
+        boolean valid = true;
+        for (int i = k; i < L; i += k) {
+            if (!s.substring(i, i + k).equals(segment)) {
+                valid = false;
+                break;
+            }
+        }
+        if (valid) return true; 
     }
+    return false;
+}
+
 
     public static void main(String[] args) throws IOException {
 
@@ -22,7 +42,11 @@ public class main {
             long upper = Long.parseLong(bounds[1]);
 
             for(long i=lower;i<=upper;i++){
-                if(isDouble(Long.toString(i))){
+                // if(isDouble(Long.toString(i))){
+                //     count+=i;
+                // }
+
+                if(isInvalid(Long.toString(i))){
                     count+=i;
                 }
             }
