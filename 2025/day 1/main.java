5a15117cb91efbf2e254d15c3c4ccf0a9e35cba9
shalineraghupathy <shalineraghupathy02@gmail.com>
2025-12-11 11:01:45 +0000

    day 1 part 2

--- 2025/day 1/main.java
+++ 2025/day 1/main.java
@@ -1,8 +1,7 @@
 import java.io.*;
 
 public class main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Working dir: " + System.getProperty("user.dir"));
+    public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new FileReader("./input.txt"));
 
@@ -13,16 +12,16 @@ public class main {
         while ((line = br.readLine()) != null) {
             char dir = line.charAt(0);                
             int value = Integer.parseInt(line.substring(1));
+            int step = (dir == 'L') ? -1 : 1;
 
-            if (dir == 'L') {
-                start = (start - value) % 100;
-            } else {
-                start = (start + value) % 100;
+            for(int i=0;i<value;i++){
+                start = (start+step)%100;
+
+                if(start<0)start+=100;
+                if(start == 0) count++;
             }
 
-            if (start < 0) start += 100; //negative wrap
-
-            if (start == 0) count++;
+         
         }
 
         br.close();
